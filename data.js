ffab0a89a48548c6a2835b09fa807ba8eb452cdd
Srishti Singh <srishtisingh102@gmail.com>
2023-12-09 00:47:27 -0500

    data polish

--- data.js
+++ data.js
@@ -3,18 +3,27 @@ const servicesData = [
         title: "Development",
         category: "frontend",
         description: [
-            'Proficient in languages such as JavaScript, Python, C++, and HTML/CSS', 
-        'Experience building web and mobile applications using frameworks like React and Django', 
-        'Strong visual design capabilities including prototyping, UI/UX, and graphic design']
+            'Experience with full website and web application development using languages like JavaScript, Python, Java', 
+            'Capable of working on both front end and back end codebases and integrating them', 
+            'Knowledge of web architecture patterns and end-to-end workflows from client to server'
+        ]
     },
     {
         title: "Backend Development",
         category: "backend",
-        description: ['String 1', 'String 2', 'String 3']
+        description: [
+            'Proficient in server-side languages like Node.js, Java Springboot, and Django for application logic', 
+            'Familiar with database management using MySQL and MongoDB for data storage and querying', 
+            'Implemented REST APIs and integrations with third party services like payment gateways and sms providers'
+        ]
     },
     {
         title: "Fullstack Development",
         category: "fullstack",
-        description: ['String 1', 'String 2', 'String 3']
+        description: [
+            'Strong grasp of HTML, CSS, and some JavaScript frameworks like React for interactive UIs', 
+            'Practice using modern design tools like Figma for prototyping and creating web/app interfaces', 
+            'Knowledge of responsiveness, accessibility, and performance best practices for great user experiences'
+        ]
     }
 ]
